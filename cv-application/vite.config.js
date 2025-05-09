5a497cf51ee3439e30b74c90dba18bd149992b89
Mang-o <164549467+mann-go@users.noreply.github.com>
2025-05-09 21:34:51 +0100

    FIX: Added a base url to vite.config.js to serve with Netlify

--- cv-application/vite.config.js
+++ cv-application/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // This points Netlify to this directory to serve as a page.
+  base: '/cv-application/',
   plugins: [react()],
 })
