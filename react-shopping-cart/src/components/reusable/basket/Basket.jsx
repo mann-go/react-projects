776710bac8b2b85f30dbce61617899ed5dd4bb68
Mang-o <164549467+mann-go@users.noreply.github.com>
2025-06-18 20:33:32 +0100

    CHORE: Updated styling for `Basket.jsx` and `Navbar.jsx`, as well as `App.css` and `index.css`.

--- react-shopping-cart/src/components/reusable/basket/Basket.jsx
+++ react-shopping-cart/src/components/reusable/basket/Basket.jsx
@@ -1,18 +1,20 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { useBasket } from "../../../contexts/BasketContext";
 import "./Basket.css";
+import Button from "../button/Button";
 
 function Basket() {
   const { basket } = useBasket();
 
   return (
-    <div className="basket">
-        {basket.length}
-      <FontAwesomeIcon icon={faShoppingCart} size="xl"/>
-    </div>
-
-  )
+    <Button
+      id="basket"
+      type="button"
+      text={basket.length}
+      icon={faShoppingCart}
+    ></Button>
+  );
 }
 
 export default Basket;
--- react-shopping-cart/src/components/reusable/nav/Navbar.jsx
+++ react-shopping-cart/src/components/reusable/nav/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <>
       <nav>
-        <h1>WEBSITE NAME</h1>
+        <h1>Shamazon</h1>
         <Link to="/"><Button id="home" type="button" text="Home"/></Link>
         <Link to="/products/all"><Button id="products" type="button" text="Products"/></Link>
         <Link to="/checkout"><Basket/></Link>
