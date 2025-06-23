a04c262571d4f2271aeb687443ff95d847c89d62
Mang-o <164549467+mann-go@users.noreply.github.com>
2025-06-23 18:16:49 +0100

    CHORE: Seperated `routes` from `main.jsx` into it's own file `routes.jsx'

--- react-shopping-cart/src/main.jsx
+++ react-shopping-cart/src/main.jsx
@@ -1,63 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-import Landing from "./components/home/landing/Landing.jsx";
-import Products from "./components/product/products/Products.jsx";
-import ErrorPage from "./components/reusable/error-page/ErrorPage.jsx";
-import { BasketProvider } from "./components/contexts/BasketContext.jsx";
-import Checkout from "./components/checkout/Checkout.jsx";
-import PageTitle from "./components/reusable/page-title/PageTItle.jsx";
+import router from "./routes.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <BasketProvider>
-        <App />
-      </BasketProvider>
-    ),
-    errorElement: (
-      <>
-        <PageTitle title="404 | Page not found" />
-        <ErrorPage />
-      </>
-    ),
-    children: [
-      {
-        index: true,
-        element: (
-          <>
-            <PageTitle title="Home Page" />
-            <Landing />
-          </>
-        ),
-      },
-      {
-        path: "products/:category",
-        element: (
-          <>
-            <PageTitle title="Products" />
-            <Products />,
-          </>
-        ),
-      },
-      {
-        path: "checkout",
-        element: (
-          <>
-            <PageTitle title="Checkout" />
-            <Checkout />,
-          </>
-        ),
-      },
-    ],
-  },
-]);
+const routes = router;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={routes} />
   </StrictMode>
 );
---
+++ react-shopping-cart/src/routes.jsx
@@ -0,0 +1,56 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Landing from "./components/home/landing/Landing.jsx";
+import Products from "./components/product/products/Products.jsx";
+import ErrorPage from "./components/reusable/error-page/ErrorPage.jsx";
+import { BasketProvider } from "./components/contexts/BasketContext.jsx";
+import Checkout from "./components/checkout/Checkout.jsx";
+import PageTitle from "./components/reusable/page-title/PageTItle.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <BasketProvider>
+        <App />
+      </BasketProvider>
+    ),
+    errorElement: (
+      <>
+        <PageTitle title="404 | Page not found" />
+        <ErrorPage />
+      </>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <>
+            <PageTitle title="Home Page" />
+            <Landing />
+          </>
+        ),
+      },
+      {
+        path: "products/:category",
+        element: (
+          <>
+            <PageTitle title="Products" />
+            <Products />,
+          </>
+        ),
+      },
+      {
+        path: "checkout",
+        element: (
+          <>
+            <PageTitle title="Checkout" />
+            <Checkout />,
+          </>
+        ),
+      },
+    ],
+  },
+]);
+
+export default router;
